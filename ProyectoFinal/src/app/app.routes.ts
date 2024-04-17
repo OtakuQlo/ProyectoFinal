ec0c675e1e81fba5f5c91385f217bd451904ec70
OtakuQlo_ <manuelenrriquerivera@gmail.com>
2024-04-17 16:01:55 -0400

    Update app.routes.ts
    Creacion de rutas

--- ProyectoFinal/src/app/app.routes.ts
+++ ProyectoFinal/src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RealizarVentaComponent } from './realizar-venta/realizar-venta.component';
+import { SeccionPerfilesComponent } from './seccion-perfiles/seccion-perfiles.component';
+import { CrearPerfilComponent } from './crear-perfil/crear-perfil.component';
+import { CrearPerfilJefeComponent } from './crear-perfil-jefe/crear-perfil-jefe.component';
+import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
 
 export const routes: Routes = [
-    { path: 'Home', component: HomeComponent }, { path: '', redirectTo: 'Home', pathMatch: 'full' }
+    { path: 'Home', component: HomeComponent }, 
+    { path: 'Registro', component: CrearCuentaComponent },
+    { path: 'CrearPerfil', component: CrearPerfilComponent },
+    { path: 'CrearJefe', component: CrearPerfilJefeComponent },  
+    { path: 'Venta', component: RealizarVentaComponent },
+    { path: 'Perfiles', component: SeccionPerfilesComponent },
+    { path: '', redirectTo: 'Home', pathMatch: 'full' }
 ];
