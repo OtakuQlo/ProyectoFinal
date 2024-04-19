02519773cd9c4d9ddefa149d467425dc218928bc
SubwayEnjoyer <107148747+SubwayEnjoyer@users.noreply.github.com>
2024-04-19 17:43:18 -0400

    basededato2

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    
+    next()
+})
+
+app.use(bodyParser.json())
+
+const PUERTO = 3000
+
+const conexion = mysql.createConnection(
+    {
+        host:'localhost',
+        database:'ordenalo',
+        user:'root',
+        password:'1234'
+    }
+)
+
+app.listen(PUERTO, () => {
+    console.log(`Servidor corriendo en el puerto ${PUERTO}`);
+})
+
+conexion.connect(error => {
+    if(error) throw error
+    console.log('Conexión exitosa a la base de datos');
+})
+
+app.get('/', (req, res) => {
+    res.send('API')
+})
+
