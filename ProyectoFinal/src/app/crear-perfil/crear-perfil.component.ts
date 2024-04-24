8fcd8dd1408b9a16c2850bf63513cca1e293a89c
SubwayEnjoyer <107148747+SubwayEnjoyer@users.noreply.github.com>
2024-04-24 19:35:42 -0400

    crear perfil ts

--- ProyectoFinal/src/app/crear-perfil/crear-perfil.component.ts
+++ ProyectoFinal/src/app/crear-perfil/crear-perfil.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <== ¡Añade las importaciones!
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-crear-perfil',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,],
   templateUrl: './crear-perfil.component.html',
   styleUrl: './crear-perfil.component.css'
 })
 export class CrearPerfilComponent {
+  nombre: string = '';
+  
+
+  labelnombre: string = '';
+  
+
+  regexname: RegExp =
+    /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]{2,100}$/;
+
+
+  crearPerfil(){
+    let bandera = true;
+
+   if (!this.regexname.test(this.nombre)) {
+     bandera = false;
+     this.labelnombre =
+       'El nombre debe ser sin caracteres epeciales ni numeros.';
+   } else {
+     this.labelnombre = '';
+   }
+
+   if (!bandera) {
+     // no avanza
+   }else{
+     // avanza
+   }
+
+ }
 
 }
