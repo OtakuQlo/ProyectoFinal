04fe3297f295793fe8571d8bcd2b1f4ff9dbe0c5
Andrés Gutiérrez <65102015+AndresGutierrezBastidas@users.noreply.github.com>
2024-05-15 23:21:42 -0400

    ñ

--- ProyectoFinal/src/environments/environment.development.ts
+++ ProyectoFinal/src/environments/environment.development.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: false,
-    // encriptar tus cuentas
-    key: '',
-    // api de tu base de datos
-    url: ''
+    key: 'romeoyjulieta',
+    url: 'http://localhost:3000/api/'
   };
