e338e2141687fcbe30c9445657f52508d1dec2b9
OtakuQlo_ <manuelenrriquerivera@gmail.com>
2024-05-20 12:18:41 -0400

    Update venta.service.ts

--- ProyectoFinal/service/venta.service.ts
+++ ProyectoFinal/service/venta.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../src/environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
