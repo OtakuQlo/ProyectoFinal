a220ae3c2f8507a1741bcab9862f3b9dfee9f1f4
OtakuQlo_ <manuelenrriquerivera@gmail.com>
2024-07-06 14:04:55 -0400

    Fixed Bug Realizar-compra
    Arreglado el tema de que no mostraba el stock bien de acuerdo a lo que ya se habia ingresado en detalle

--- ProyectoFinal/src/app/realizar-venta/realizar-venta.component.ts
+++ ProyectoFinal/src/app/realizar-venta/realizar-venta.component.ts
@@ -19,8 +19,8 @@ import { MailService } from '../../../service/mail.service';
 })
 export class RealizarVentaComponent {
 
-  constructor( private venta: VentaService, private alert: ToastService, private perfil: PerfilusuarioService, private productoS: ProductoService, private oldstock: OldstocksService, private correo: MailService) {
-    
+  constructor(private venta: VentaService, private alert: ToastService, private perfil: PerfilusuarioService, private productoS: ProductoService, private oldstock: OldstocksService, private correo: MailService) {
+
   }
 
   ngOnInit(): void {
@@ -38,6 +38,7 @@ export class RealizarVentaComponent {
   //Producto
   codebar: string = '';
   cantidad: number = 0;
+  CA: any;
 
   //Perfil
   perfilV: any = this.perfil.getPerfilActivo()
@@ -51,8 +52,8 @@ export class RealizarVentaComponent {
   labelcantidad: string = '';
 
 
-  stockproducto : any;
-  oldstockproducto:any;
+  stockproducto: any;
+  oldstockproducto: any;
 
   valVenta() {
     let bandera = true;
@@ -76,10 +77,10 @@ export class RealizarVentaComponent {
   }
 
   async pagarVenta() {
-    if(this.total > 0){
+    if (this.total > 0) {
       let boletin = this.boleta[0]
       boletin.preciototal = this.total
-      await this.venta.actualizarBoleta(boletin.idboleta, {"preciototal" : boletin.preciototal}).subscribe()
+      await this.venta.actualizarBoleta(boletin.idboleta, { "preciototal": boletin.preciototal }).subscribe()
       for (let index = 0; index < this.detalle.length; index++) {
         const element = this.detalle[index];
         this.venta.realizarCompra({
@@ -88,58 +89,63 @@ export class RealizarVentaComponent {
           "idproducto": element.idproducto.idproducto,
           "cantidad": element.cantidad
         }).subscribe({
-          next:(data)=>{
-            let producto : any;
-            
+          next: (data) => {
+            let producto: any;
+
             producto = data;
-            this.oldstock.getStock(producto.idproducto).subscribe((data) =>{
+            this.oldstock.getStock(producto.idproducto).subscribe((data) => {
               this.stockproducto = data;
-              
+
 
               this.oldstock.getOldStock(this.stockproducto.idstock).subscribe((data) => {
                 this.oldstockproducto = data;
-                
+
 
                 if (this.oldstockproducto.estado == 0 && this.oldstockproducto.porcentaje <= 30) {
                   let nombreproducto: any;
-                  this.productoS.getProductoId(producto.idproducto).subscribe((data) =>{
+                  this.productoS.getProductoId(producto.idproducto).subscribe((data) => {
                     nombreproducto = data.nombreproducto;
-              
-                    this.correo.correoStock({producto : nombreproducto}).subscribe();
-                    this.oldstock.actualizarEstado(this.oldstockproducto.id,this.stockproducto).subscribe();
+
+                    this.correo.correoStock({ producto: nombreproducto }).subscribe();
+                    this.oldstock.actualizarEstado(this.oldstockproducto.id, this.stockproducto).subscribe();
                   })
                 }
               })
             })
 
-            
-            
-            
+
+
+
           }
         })
       }
-      
+
       this.cancelarPago()
       window.location.reload()
     }
   }
 
   agregarProducto() {
-    this.productoS.getProductoVenta(this.codebar,{idusuario : this.perfilV.idusuario}).subscribe(res => {
+    this.productoS.getProductoVenta(this.codebar, { idusuario: this.perfilV.idusuario }).subscribe(res => {
       this.producto = res
       if (this.producto[0] && this.producto[0].precio !== undefined) {
-        if(parseInt(this.producto[0].StockProducts[0].cantidadtotal) > 0 && parseInt(this.producto[0].StockProducts[0].cantidadtotal) >= this.cantidad){
-          this.total = this.total + (this.producto[0].precio * this.cantidad);
-          let curr = this.detalle.find(p => p.idproducto.idproducto === this.producto[0].idproducto)
-          let index = this.detalle.findIndex(obj => obj.idproducto.idproducto === this.producto[0].idproducto)
-          if (curr != undefined) {
+        let curr = this.detalle.find(p => p.idproducto.idproducto === this.producto[0].idproducto)
+        let index = this.detalle.findIndex(obj => obj.idproducto.idproducto === this.producto[0].idproducto)
+        if (curr != undefined) {
+          this.CA = (parseInt(this.producto[0].StockProducts[0].cantidadtotal) - this.detalle[index].cantidad)
+          if (parseInt(this.producto[0].StockProducts[0].cantidadtotal) > 0 && this.CA >= this.cantidad) {
             this.detalle[index].cantidad = this.detalle[index].cantidad + this.cantidad
-            this.venta.actualizarDetalle(this.detalle[index].idboleta, {cantidad : this.detalle[index].cantidad ,idproducto : this.detalle[index].idproducto.idproducto})
+            this.venta.actualizarDetalle(this.detalle[index].idboleta, { cantidad: this.detalle[index].cantidad, idproducto: this.detalle[index].idproducto.idproducto })
             this.cancelarVenta()
             this.alert.showSuccess('', 'Detalle Actualizado');
-          } 
-          if(curr == undefined) {
-            this.boletaVenta().then(()=>{
+            this.total = this.total + (this.producto[0].precio * this.cantidad)
+          } else {
+            this.alert.errorSuccess('', 'El Producto tiene un stock actual de: ' + this.CA)
+          }
+        } else if (curr == undefined) {
+          this.CA = parseInt(this.producto[0].StockProducts[0].cantidadtotal)
+          if (parseInt(this.producto[0].StockProducts[0].cantidadtotal) > 0 && this.CA >= this.cantidad) {
+            this.boletaVenta().then(() => {
               this.detalle.push({
                 iddetalle: '',
                 idboleta: this.boleta[0].idboleta,
@@ -149,21 +155,20 @@ export class RealizarVentaComponent {
               this.alert.showSuccess('', 'Producto Agregado');
               this.cancelarVenta()
             })
+          } else {
+            this.alert.errorSuccess('', 'El Producto tiene un stock actual de: ' + this.CA)
           }
-        }else{
-          this.alert.errorSuccess('','El Producto tiene un stock actual de: '+this.producto[0].StockProducts[0].cantidadtotal)
+        } else {
+          this.alert.errorSuccess('', 'Producto no encontrado');
         }
-      } else {
-        this.alert.errorSuccess('', 'Producto no encontrado');
       }
-      
     })
   }
 
-  borrarDetalles(idp : any){
+  borrarDetalles(idp: any) {
     let index = this.detalle.findIndex(detallin => detallin.idproducto.idproducto === idp);
     let borrado = this.detalle.find(detallin => detallin.idproducto.idproducto === idp)
-    this.detalle.splice(index,1)
+    this.detalle.splice(index, 1)
     this.total = this.total - (borrado.idproducto.precio * borrado.cantidad);
   }
 
@@ -174,10 +179,10 @@ export class RealizarVentaComponent {
   }
 
   async boletaVenta() {
-    await this.venta.getboleta(this.perfilV.idusuario,{nombre: this.perfilV.nombre}).then(boleta =>{
+    await this.venta.getboleta(this.perfilV.idusuario, { nombre: this.perfilV.nombre }).then(boleta => {
       this.boleta = boleta
-      if (this.boleta.length === 0){
-        let datebol = formatDate(new Date(), 'yyyy-MM-dd', 'en')        
+      if (this.boleta.length === 0) {
+        let datebol = formatDate(new Date(), 'yyyy-MM-dd', 'en')
         this.venta.crearBoleta({
           "idboleta": '',
           "nombre": this.perfilV.nombre,
